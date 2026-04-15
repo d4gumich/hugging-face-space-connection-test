baaf5f49a03267180bf20ba4e773f502adfbe00e
greciamaci <greciam@umich.edu>
2026-04-15 01:02:30 -0400

    Add lighthouse component

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+    import LighthouseResults from "../lib/components/lighthouse_results.svelte";
+
     // Change host_url based on where backend is running (i.e. local vs pythonanywhere)
     const host_url = 'http://127.0.0.1:5000/';
     const base_path = 'api/v1/products/';
@@ -7,6 +9,9 @@
     const lighthouse_path = 'lighthouse/parse-pdf';
     const url = host_url + base_path + lighthouse_path;
     let file = null;
+    // status_active is hard coded in the meantime (still deciding backend workflow)
+    let status_active = true;
+    let extracted_texts = $state(null);
 
     // Handle file selection
     function handleFileChange(event) {
@@ -39,9 +44,12 @@
             // and receive results as opposed to them having to click a separate button to wake the space)
             // 2) Receive and display information through svelte component 
             // 3) Add visualization that articulates steps to user with waiting wheel 
+            // 4) Add this frontend code to official site 
 
             const result = await response.json();
             alert(`Upload successful: ${result.extracted_text}`);
+            extracted_texts = result.extracted_text;
+            console.log(extracted_texts);
         } catch (error) {
             console.error("Upload failed:", error);
             alert("Failed to upload PDF.");
@@ -54,3 +62,23 @@
     <input type="file" accept="application/pdf" onchange={handleFileChange} />
     <button onclick={uploadPDF}>Upload PDF</button>
 </div>
+
+<div class="results-container">
+            {#if status_active}
+                {#if extracted_texts}
+                    <div class="output-container">
+                        <div class="output">
+                                <LighthouseResults extracted_text={extracted_texts}/>
+                        </div>
+                    </div>
+                    {/if}
+            {:else}
+                <p>
+                    <strong>The Hugging Face Space is inactive</strong>
+                </p>
+            {/if}
+    </div>
+<!-- Depending on the backend workflow the below code will need to change for
+the results to display -->
+
+
