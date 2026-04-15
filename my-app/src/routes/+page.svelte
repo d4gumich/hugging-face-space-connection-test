663372d890cc51164c11a46c66ddd1eff7ecb703
greciamaci <greciam@umich.edu>
2026-04-14 21:17:59 -0400

    Add updated code that connects to backend

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script>
-    const host_url = 'https://d4gumsi.pythonanywhere.com/';
-    const endpoint = 'api/v2/products/lighthouse';
-    const url = host_url + endpoint;
+    // Change host_url based on where backend is running (i.e. local vs pythonanywhere)
+    const host_url = 'http://127.0.0.1:5000/';
+    const base_path = 'api/v1/products/';
+    // If needed, update the below lighthouse path depending on what backend API will
+    // be exposed for ingesting pdf
+    const lighthouse_path = 'lighthouse/parse-pdf';
+    const url = host_url + base_path + lighthouse_path;
     let file = null;
 
     // Handle file selection
@@ -9,7 +13,7 @@
         file = event.target.files[0];
     }
 
-    // Send PDF to PythonAnywhere backend
+    // Send PDF to backend as formdata 
     async function uploadPDF() {
         if (!file) {
             alert("Please select a PDF file first.");
@@ -17,7 +21,7 @@
         }
 
         const formData = new FormData();
-        formData.append("pdf", file);
+        formData.append("file", file);
 
         try {
             const response = await fetch(url, {
@@ -29,13 +33,15 @@
                 throw new Error(`Server error: ${response.status}`);
             }
 
-            //TODO: 
-            // 1) Determine what format is returned 
-            // 2) Add a check for hugging face space status 
-            // 3) Articulate steps to user with waiting wheel 
+            // Next steps for team: 
+            // 1) (Suggested) Add hugging face space status check incorporated
+            // as part of the backend work flow automatically (the user ONLY has to upload file 
+            // and receive results as opposed to them having to click a separate button to wake the space)
+            // 2) Receive and display information through svelte component 
+            // 3) Add visualization that articulates steps to user with waiting wheel 
 
             const result = await response.json();
-            alert(`Upload successful: ${result.filename}`);
+            alert(`Upload successful: ${result.extracted_text}`);
         } catch (error) {
             console.error("Upload failed:", error);
             alert("Failed to upload PDF.");
